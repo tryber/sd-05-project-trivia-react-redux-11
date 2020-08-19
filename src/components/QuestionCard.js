72d59700df087167c15c675e10a7bb3839e01ad3
hleoc <informig@gmail.com>
2020-08-19 18:44:18 -0300

    Botão próximo

--- src/components/QuestionCard.js
+++ src/components/QuestionCard.js
@@ -1,17 +1,43 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import changePosition from '../action/changePosition';
 
 class QuestionCard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    const { questionPosition, questions, changePositions } = this.props;
+    if (questionPosition < questions.length - 1) {
+      changePositions();
+    }
+  }
+
   render() {
+    const i = this.props.questionPosition;
     console.log(this.props);
     if (!this.props.questions) return <div> Carregando Perguntas ...</div>;
-    const { type, category, question } = this.props.questions[0];
+    const { type, category, question } = this.props.questions[i];
+    const correctAnswer = this.props.questions[i].correct_answer;
+    const incorrectAnswers = this.props.questions[i].incorrect_answers;
     return (
       <div>
-        {type}
-        {category}
-        {question}
+        <div>{type}</div>
+        <div data-testid="question-category">{category}</div>
+        <div data-testid="question-test" >{question}</div>
+        <div>
+          <button data-testid="correct-answer">{correctAnswer}</button>
+          {incorrectAnswers.map((answer, index) =>
+            <button data-testid={`wrong-answer-${index}`}>{answer}</button>,
+          )}
+        </div>
+        <button data-testid="btn-next" onClick={this.handleClick}>Próximo</button>
       </div>
     );
   }
@@ -20,10 +46,17 @@ class QuestionCard extends React.Component {
 const mapStateToProps = (state) => ({
   isFetching: state.token.isFetching,
   questions: state.questions.questions.results,
+  questionPosition: state.questions.questionPosition,
 });
 
-export default connect(mapStateToProps)(QuestionCard);
+const mapDispatchToProps = (dispatch) => ({
+  changePositions: () => dispatch(changePosition()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(QuestionCard);
 
 QuestionCard.propTypes = {
   questions: PropTypes.arrayOf(PropTypes.object).isRequired,
+  questionPosition: PropTypes.number.isRequired,
+  changePositions: PropTypes.func.isRequired,
 };
