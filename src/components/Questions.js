63a1cb51ec2772f61438185f563485353d8c456a
hleoc <informig@gmail.com>
2020-08-20 20:26:01 -0300

    Sorteando a ordem das respostas - Corrigindo erros

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import QuestionCard from '../components/QuestionCard';
 
@@ -31,3 +32,7 @@ const mapStateToProps = (state) => ({
 });
 
 export default connect(mapStateToProps)(Questions);
+
+Questions.propTypes = {
+  score: PropTypes.number.isRequired,
+};
