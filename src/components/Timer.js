c94f016bcede0522e9264c0ae5cd29ec29a4186c
Lucas Allan Almeida Oliveira <lucas11allan@gmail.com>
2020-08-20 22:06:51 -0300

    temporizador

---
+++ src/components/Timer.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+class Timer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 30,
+    };
+  }
+
+  componentDidMount() {
+    if (this.state.count > 0) {
+      this.myInterval = setInterval(() => { 
+        this.setState(prevState => ({ count: prevState.count - 1 }))
+      }, 1000);
+    }
+  }
+
+  componentWillUnmount () {
+    clearInterval(this.myInterval);
+  }
+
+  render() {
+    return (
+      <div>
+        Tempo:{this.state.count}
+      </div>
+    );
+  }
+}
+
+export default Timer;
