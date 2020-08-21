79a518394bdf6dd81ea980509b4e2c54d6ebb37e
Jorge Lucas <jorge.lucas2264@gmail.com>
2020-08-21 11:25:20 -0300

    Ranking Screen added

--- src/components/Ranking.js
+++ src/components/Ranking.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 class Ranking extends React.Component {
   constructor(props) {
@@ -10,16 +11,29 @@ class Ranking extends React.Component {
   }
 
   render() {
+    const hash = localStorage.getItem('EmailMD5');
     return (
       <div>
-        Ranking
+        <header>
+          Ranking:
+        </header>
+        <img src={`https://www.gravatar.com/avatar/${hash}`} alt="Avatar" />
+        <div>
+          {localStorage.getItem('name')}
+        </div>
+        <div>
+          Score: {this.props.score}
+        </div>
+        <Link to="/">
+          <button data-testid="btn-go-home">Início</button>
+        </Link>
       </div>
     );
   }
 }
 
 const mapStateToProps = (state) => ({
-  isFetching: state.token.isFetching,
+  score: state.player.score,
 });
 
 const mapDispatchToProps = () => ({
